e3d78d16ea4621f6d23200d96ba07e29c8799e04
BlackFloat <lin70208@gmail.com>
2021-09-29 14:09:26 +0800

    added readme added new function:select option for convert mac address added demo extension gif

---
+++ README.md
@@ -0,0 +1,6 @@
+# Mac address convert extension for Visual Studio Code
+
+This extension helps you to easily transfer your mac address to different format.
+![Demo toggle for convert mac address](./demoToggle.gif)<br>
+
+![Demo Select option for convert mac address](./demoSelect.gif)
--- extension.js
+++ extension.js
@@ -1,43 +1,61 @@
 const vscode = require("vscode");
 
-function macAddressAddSemi(text) { 
-  return text.match(/.{1,2}/g).join(":")
+// regex for not have symbol mac address
+const regMacAddressNoSymbol = /^([a-fA-F0-9]{2}){6}$/
+
+// regex for have symbol mac address
+const regMacAddressSymbol = /^([a-fA-F0-9]{2}[:\.;-]){5}[a-fA-F0-9]{2}$/
+
+// add symbol for mac address
+// example: 1234567901A => 12:34:56:78:90:1A
+function macAddressAddSymbol(text,symbol = ':') { 
+  return text.match(/.{1,2}/g).join(symbol)
 }
 
-function macAddressRemoveSemi(text) {
+// remove symbol for mac address
+// example: 12:34:56:78:90:1A => 1234567901A
+function macAddressRemoveSymbol(text) {
   return text.split(/[:\.;-]/).join("")
 }
+
 
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-  let toggleConvertMacAddress = vscode.commands.registerCommand(
-    "test.toggleMacAddress",
+  let toggleConvert = vscode.commands.registerCommand(
+    "test.toggleConvert",
     () =>{
+      // Step: 
+      // 1. get select text,then split("\r\n") into lines
+      // 2. for loop lines to line, and check every line whether have valid mac address
+      // 3. if valid ,then check have symbol(/:.;-/)
+      // 4. if have symbol, remove symbol
+      // 6. otherwise, add symbol(:)
+      // 5. return finish covert result,
+      //    and show notification whether mac address have convert success.
+
       const editor = vscode.window.activeTextEditor;
       const selection = editor.selection;
       const selectText = editor.document.getText(selection);
 
       const lines = selectText.split('\n')
 
-      let notMacAddressList = []
+      let errorList = []
 
-      let newLines = lines.map((line,index)=> {
+      let newLines = lines.map((line)=> {
         line = line.trim()
 
-        let regMacAddressNoSemi = /^([a-fA-F0-9]{2}){6}$/
+        if(regMacAddressNoSymbol.test(line)) {
+          return macAddressAddSymbol(line)
+        } else if(regMacAddressSymbol.test(line)) {
+          return macAddressRemoveSymbol(line)
+        } else if (line.length > 0){
+          errorList.push(line)
 
-        let regMacAddressSemi = /^([a-fA-F0-9]{2}[:\.;-]){5}[a-fA-F0-9]{2}$/
-
-        if(regMacAddressNoSemi.test(line)) {
-          return `${macAddressAddSemi(line)}`
-        } else if(regMacAddressSemi.test(line)) {
-          return `${macAddressRemoveSemi(line)}`
+          return line
         } else {
-          notMacAddressList.push(index)
-
           return line
         }
       })
@@ -46,21 +64,110 @@ function activate(context) {
 
       editor.edit(builder => builder.replace(selection, result))
 
-      let infoMessage = "Replace Mac Address success"
+      let infoMessage = "Convert Mac Address success"
 
-      if(notMacAddressList.length > 0) {
-        infoMessage = `At line:${notMacAddressList.map(item => item + 1).join(',')} is not mac address`
+      if(errorList.length > 0 ) {
+        infoMessage = `Convert mac Address fail,
+        invalid mac address:${errorList.join(",")}`
       }
 
       vscode.window.showInformationMessage(infoMessage);
     }
   );
 
+  
+  context.subscriptions.push(toggleConvert);
 
-  context.subscriptions.push(toggleConvertMacAddress);
+  let selectConvert = vscode.commands.registerCommand(
+    "test.selectConvert",
+    () =>{
+      // Step: 
+      // 1. get user select option
+      // 2. option is remove symbol,and if mac address is valid ,then do remove symbol 
+      //    option is added symbol,and if mac address is valid ,then do added symbol
+      // 3. return finish covert result,
+      //    and show notification whether mac address have convert success.
+      const editor = vscode.window.activeTextEditor;
+      const selection = editor.selection;
+      const selectText = editor.document.getText(selection);
+
+      const lines = selectText.split('\n')
+
+      let errorList = []
+
+      let pickItems = [
+        {
+          label: "Remove Symbol"
+        },
+        {
+          symbol:':',
+          label: "Add symbol(:)"
+        },
+        {
+          symbol:'-',
+          label: "Add symbol(-)"
+        },
+        {
+          symbol:'.',
+          label: "Add symbol(.)"
+        },
+        {
+          symbol:';',
+          label: "Add symbol(;)"
+        },
+      ]
+
+      vscode.window.showQuickPick(pickItems,{
+      }).then(item => {
+        let newLines = lines.map((line)=> {
+          line = line.trim()
+  
+          if(item.symbol) {
+            if(regMacAddressNoSymbol.test(line)) {
+              return macAddressAddSymbol(line,item.symbol)
+            } else if(regMacAddressSymbol.test(line)){
+              const noSymBolMacAddress = macAddressRemoveSymbol(line)
+
+              return macAddressAddSymbol(noSymBolMacAddress,item.symbol)
+            }  else if (line.length > 0){
+              errorList.push(line)
+
+              return line
+            } else {
+              return line
+            }
+          } else {
+            if(regMacAddressSymbol.test(line)) {
+              return macAddressRemoveSymbol(line)
+            } else if (line.length > 0){
+              errorList.push(line)
+
+              return line
+            } else {
+              return line
+            }
+          }
+        })
+
+        const result = newLines.join("\n")
+
+        editor.edit(builder => builder.replace(selection, result))
+
+        let infoMessage = "Convert Mac Address success"
+
+        if(errorList.length > 0 ) {
+          infoMessage = `Convert mac Address fail,
+          invalid mac address:${errorList.join(",")}`
+        }
+
+        vscode.window.showInformationMessage(infoMessage);
+      })
+    }
+  );
+
+  context.subscriptions.push(selectConvert);
 }
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
